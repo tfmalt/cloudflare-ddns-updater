557bdfcdfc7d34fe133d7b2031032d5f39b9368b
Thomas Malt <thomas@malt.no>
2017-01-15 16:50:08 +0100

    adding latest updates

--- cloudflare.js
+++ cloudflare.js
@@ -7,52 +7,99 @@
  * @license MIT
  */
 
-const request = require("request-promise-native");
 const api     = require("./config").api;
 const hosts   = require("./config").hosts;
+const version = require("./package").version;
+const request = require("request-promise-native");
 const net     = require("network-address");
-const w     = require("winston");
+const winston     = require("winston");
 
-api.key = api.key || process.env.CLOUDFLARE_AUTH_KEY;
+var w = new (winston.Logger)({
+  level: "debug",
+  transports: [
+    new (winston.transports.Console)({
+      "timestamp": true
+    })
+  ]
+});
+
+// Setting up the API access defaults.
+api.key   = api.key   || process.env.CLOUDFLARE_AUTH_KEY;
 api.email = api.email || process.env.CLOUDFLARE_AUTH_EMAIL;
+api.zones = api.zones || process.env.CLOUDFLARE_ZONES;
+
+w.cli();
+w.info(`Starting cloudfare DDNS updater v${version}`);
+w.info("Updating the following hosts:");
+for (let host of Object.keys(hosts)) {
+  w.info(`  ${host} - ${hosts[host].types}`);
+}
 
 /**
  * Object literal to keep the cloudflare helper functions
  */
 const cf = {
+  uriroot: `https://api.cloudflare.com/client/v4/zones/${api.zones}/dns_records`,
   headers: {
     "Content-Type": "application/json",
     "X-Auth-Key": api.key,
     "X-Auth-Email": api.email
-  }
+  },
+  seen: {}
 };
 
 /**
  * Start of stubs to update the DNS records
  */
-cf.handleRecord = function (data) {
-  console.log("Told to handle record for:", data.host.name);
-  console.log(data);
-
+cf.handleDNSRecord = function (data) {
   if (data.types.includes(data.type)) {
-    cf.updateRecord(data);
+    cf.updateDNSRecord(data);
   } else {
-    cf.deleteRecord(data);
+    cf.deleteDNSRecord(data);
   }
-
 };
 
-cf.updateRecord = function (data) {
-  console.log("Told to update record: " + data.host.name);
+/**
+ * Updates an exisitng DNS record on cloudflare
+ */
+cf.updateDNSRecord = (data) => {
+  w.info(`    told to update record: ${data.host.name} - ${data.host.type}`);
+  w.debug(JSON.stringify(data));
+
+  return request({
+    method: "PUT",
+    uri: `${cf.uriroot}/${data.host.id}`,
+    headers: cf.headers,
+    body: {
+      type: data.type,
+      name: data.host.name,
+      content: data.addr[data.host.type]
+    },
+    json: true
+  })
+  .then( (res) => {
+    w.info("result:", res);
+    if (res.success === true) {
+      w.info("      Update successful!");
+    }
+    else {
+      w.error("     Update failed!");
+      w.debug(JSON.stringify(res));
+      process.exit(1);
+    }
+  })
+  .catch( (error) => {
+    w.error("got error in updateDNSRecord:", error.message, error);
+    process.exit(1);
+  });
 };
 
-cf.deleteRecord = function (data) {
-  console.log("Told to delete record: " + data.host.name);
+
+cf.deleteDNSRecord = function (data) {
+  w.info("Told to delete record: " + data.host.name);
   request({
     method: "DELETE",
-    uri: "https://api.cloudflare.com/client/v4/zones/"
-      + "3d086ca596f0c9d22c3f99d502b93f2d/dns_records/"
-      + data.host.id,
+    uri: `${cf.uriroot}/${data.host.id}`,
     headers: cf.headers,
     json: true
   })
@@ -64,44 +111,169 @@ cf.deleteRecord = function (data) {
   });
 };
 
-cf.fetchIPv4Addr = function () {
-  return request("https://ipinfo.io/ip").then( (addr) => addr.trim() );
-};
+/**
+ * Process the received records.
+ */
+cf.iterateOverRecords = (records) => {
+  let results = {
+    data: records,
+    updates: []
+  };
 
-cf.fetchIPv4Addr().then( (addr) => {
-  return request({
-    "uri": "https://api.cloudflare.com/client/v4/zones/3d086ca596f0c9d22c3f99d502b93f2d/dns_records",
-    "headers": {
-      "Content-Type": "application/json",
-      "X-Auth-Key": api.key,
-      "X-Auth-Email": api.email
-    },
-    "json": true
-  })
-  .then( (data) => {
-    return {
-      "addr": {
-        "A": addr,
-        "AAAA": net.ipv6()
-      },
-      "data": data
-    };
-  });
-})
-.then((data) => {
-  for (let item of data.data.result) {
-    if(hosts.hasOwnProperty(item.name)) {
-      if (hosts[item.name].types.includes(item.type) === false) {
-        cf.handleRecord({
-          addr: data.addr,
+  for (let item of records.data.result) {
+    if(hosts.hasOwnProperty(item.name) === false) {
+      continue;
+    }
+
+    w.info(`  Processing ${item.name} - ${item.type}`);
+
+    if (hosts[item.name].types.includes(item.type)) {
+      if (item.content === records.addr[item.type]) {
+        w.info("    addresses equal - Nothing to do");
+      }
+      else {
+        w.info("    addresses not equal - updating record");
+        results.updates[results.updates.length] = cf.updateDNSRecord({
+          addr: records.addr,
           type: item.type,
           types: hosts[item.name].types,
           host: item
+        })
+        .then( (res) => {
+          w.info("    update done:", JSON.stringify(res));
         });
       }
+
+      if (!cf.seen.hasOwnProperty(item.name)) {
+        cf.seen[item.name] = {};
+      }
+      cf.seen[item.name][item.type] = 1;
+
+      continue;
+    } else {
+      w.info("    record has wrong type - dealing with it");
+      results.updates[results.updates.length] = cf.handleDNSRecord({
+        addr: records.addr,
+        type: item.type,
+        types: hosts[item.name].types,
+        host: item
+      });
     }
   }
-})
+
+  return results;
+};
+
+/**
+ * Fetches the external IPv4 address from one of the service providers
+ * available
+ *
+ * @return Promise
+ */
+cf.fetchIPv4Addr = () => {
+  w.info("Fetching external IP address:");
+
+  return request("https://ipinfo.io/ip")
+    .then( (addr) => addr.trim() )
+    .then( (addr) => {
+      let addresses = {
+        "A": addr,
+        "AAAA": net.ipv6()
+      };
+      w.info(`     A - ${addresses.A}`);
+      w.info(`  AAAA - ${addresses.AAAA}`);
+
+      return addresses;
+    })
+    .catch( (err) => {
+      w.error("Got an error fetching external IP addresses:", err.message);
+      process.exit(1);
+    });
+};
+
+cf.createDNSRecord = (data) => {
+  request({
+    method: "POST",
+    uri: cf.uriroot,
+    headers: cf.headers,
+    body: data,
+    json: true
+  })
+  .then( (res) => {
+    w.info("Created DNS Record: ", JSON.stringify(res));
+  })
+  .catch( (err) => {
+    w.error("Got error from createDNSRecord:", err.message);
+  });
+};
+
+cf.addMissingRecords = (data) => {
+  w.info("Adding missing records");
+  w.debug("  seen:", JSON.stringify(cf.seen));
+  w.debug("  hosts", JSON.stringify(hosts));
+
+  let results = {
+    "data": data,
+    "updates": []
+  };
+
+  for (let key of Object.keys(hosts)) {
+    w.debug("  key: ", key);
+    if (cf.seen.hasOwnProperty(key)) {
+      for (let type of hosts[key].types) {
+        if (cf.seen[key].hasOwnProperty(type)) {
+          w.debug(`    ${type} record in place - nothing to do`);
+        } else {
+          w.debug(`    ${type} record missing - adding record`);
+          results.updates[results.updates.length] = cf.createDNSRecord({
+            "type": type,
+            "name": key,
+            "content": data.records.addr[type]
+          });
+        }
+      }
+    }
+    else {
+      w.debug(`Did not find any record for ${key} - adding records ${hosts[key].types}`);
+    }
+  }
+
+  w.info("Done processing DNS records");
+  return results;
+};
+
+/**
+ * @return Promise with JSON from cloudflare.
+ */
+cf.fetchDNSRecords = (addr) => {
+  w.info("Fetching info about existing DNS records");
+
+  return request({
+    method: "GET",
+    uri: cf.uriroot,
+    headers: cf.headers,
+    json: true
+  })
+  .then( (data) => {
+    return {
+      "addr": addr,
+      "data": data
+    };
+  })
+  .catch( (err) => {
+    w.error("Could not fetch DNS Records:", err.statusCode);
+    for (let e of err.error.errors) {
+      w.error(`  ${e.code} - ${e.message}`);
+    }
+    process.exit(1);
+  });
+};
+
+cf.fetchIPv4Addr()
+.then(cf.fetchDNSRecords)
+.then(cf.iterateOverRecords)
+.then(cf.addMissingRecords)
 .catch((error) => {
+  console.log(error);
   console.log("got error:", error.message);
 });
